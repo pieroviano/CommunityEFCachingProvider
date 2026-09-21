e94b9f544e92db0e21553a59edc53d912432c604
pieroviano <piero.viano@gmail.com>
2026-09-22 02:10:35 +0300

    Convert EFCachingProvider.Tests from NUnit to xUnit

--- Code/EFCachingProvider.Tests/InMemoryCacheTests.cs
+++ Code/EFCachingProvider.Tests/InMemoryCacheTests.cs
@@ -5,53 +5,52 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using EFCachingProvider.Caching;
-using NUnit.Framework;
+using Xunit;
 
 namespace EFCachingProvider.Tests
 {
-    [TestFixture]
     public class InMemoryCacheTests
     {
-        [Test]
+        [Fact]
         public void InMemoryCacheInitialStatisticsAreAllZero()
         {
             InMemoryCache cache = new InMemoryCache();
-            Assert.AreEqual(0, cache.CacheHits);
-            Assert.AreEqual(0, cache.CacheItemAdds);
-            Assert.AreEqual(0, cache.CacheItemInvalidations);
-            Assert.AreEqual(0, cache.CacheMisses);
+            Assert.Equal(0, cache.CacheHits);
+            Assert.Equal(0, cache.CacheItemAdds);
+            Assert.Equal(0, cache.CacheItemInvalidations);
+            Assert.Equal(0, cache.CacheMisses);
         }
 
-        [Test]
+        [Fact]
         public void InMemoryCacheCacheMissTests()
         {
             InMemoryCache cache = new InMemoryCache();
             object value;
 
-            Assert.IsFalse(cache.GetItem("NoSuchItem", out value));
-            Assert.IsNull(value);
-            Assert.AreEqual(0, cache.CacheHits);
-            Assert.AreEqual(1, cache.CacheMisses);
-            Assert.AreEqual(0, cache.CacheItemInvalidations);
-            Assert.AreEqual(0, cache.CacheItemAdds);
+            Assert.False(cache.GetItem("NoSuchItem", out value));
+            Assert.Null(value);
+            Assert.Equal(0, cache.CacheHits);
+            Assert.Equal(1, cache.CacheMisses);
+            Assert.Equal(0, cache.CacheItemInvalidations);
+            Assert.Equal(0, cache.CacheItemAdds);
         }
 
-        [Test]
+        [Fact]
         public void InMemoryCacheCacheHitTests()
         {
             InMemoryCache cache = new InMemoryCache();
             object value;
 
             cache.PutItem("Item1", "someValue", new string[0], TimeSpan.Zero, DateTime.MaxValue);
-            Assert.IsTrue(cache.GetItem("Item1", out value));
-            Assert.AreEqual(value, "someValue");
-            Assert.AreEqual(1, cache.CacheHits);
-            Assert.AreEqual(0, cache.CacheMisses);
-            Assert.AreEqual(0, cache.CacheItemInvalidations);
-            Assert.AreEqual(1, cache.CacheItemAdds);
+            Assert.True(cache.GetItem("Item1", out value));
+            Assert.Equal(value, "someValue");
+            Assert.Equal(1, cache.CacheHits);
+            Assert.Equal(0, cache.CacheMisses);
+            Assert.Equal(0, cache.CacheItemInvalidations);
+            Assert.Equal(1, cache.CacheItemAdds);
         }
 
-        [Test]
+        [Fact]
         public void InMemoryCacheCacheReplaceTests()
         {
             InMemoryCache cache = new InMemoryCache();
@@ -60,16 +59,16 @@ namespace EFCachingProvider.Tests
 
             cache.PutItem("Item1", "someValue", new string[0], TimeSpan.Zero, DateTime.MaxValue);
             cache.PutItem("Item1", "someOtherValue", new string[0], TimeSpan.Zero, DateTime.MaxValue);
-            Assert.IsTrue(cache.GetItem("Item1", out value));
-            Assert.AreEqual(value, "someOtherValue");
-            Assert.AreEqual(1, cache.CacheHits);
-            Assert.AreEqual(0, cache.CacheMisses);
-            Assert.AreEqual(1, cache.CacheItemInvalidations);
-            Assert.AreEqual(2, cache.CacheItemAdds);
+            Assert.True(cache.GetItem("Item1", out value));
+            Assert.Equal(value, "someOtherValue");
+            Assert.Equal(1, cache.CacheHits);
+            Assert.Equal(0, cache.CacheMisses);
+            Assert.Equal(1, cache.CacheItemInvalidations);
+            Assert.Equal(2, cache.CacheItemAdds);
         }
 
 
-        [Test]
+        [Fact]
         public void InMemoryCacheCacheExpirationTest1()
         {
             InMemoryCache cache = new InMemoryCache();
@@ -77,24 +76,24 @@ namespace EFCachingProvider.Tests
             object value;
 
             // current time is 10:00
-            cache.GetCurrentDate = () => new DateTime(2009,1,1,10,0,0);
+            cache.GetCurrentDate = () => new DateTime(2009, 1, 1, 10, 0, 0);
 
             // set expiration time to 11:00
             cache.PutItem("Item1", "someValue", new string[0], TimeSpan.Zero, new DateTime(2009, 1, 1, 11, 0, 0));
 
             // make sure the item is still there
             cache.GetCurrentDate = () => new DateTime(2009, 1, 1, 10, 59, 59);
-            Assert.IsTrue(cache.GetItem("Item1", out value));
-            Assert.AreEqual(value, "someValue");
-            Assert.AreEqual(0, cache.CacheItemInvalidations);
+            Assert.True(cache.GetItem("Item1", out value));
+            Assert.Equal(value, "someValue");
+            Assert.Equal(0, cache.CacheItemInvalidations);
 
             // make sure the item gets evicted at 11:00
             cache.GetCurrentDate = () => new DateTime(2009, 1, 1, 11, 00, 00);
-            Assert.IsFalse(cache.GetItem("Item1", out value));
-            Assert.AreEqual(1, cache.CacheItemInvalidations);
+            Assert.False(cache.GetItem("Item1", out value));
+            Assert.Equal(1, cache.CacheItemInvalidations);
         }
 
-        [Test]
+        [Fact]
         public void InMemoryCacheCacheExpirationTest2()
         {
             InMemoryCache cache = new InMemoryCache();
@@ -109,28 +108,28 @@ namespace EFCachingProvider.Tests
 
             // make sure the item is still there
             cache.GetCurrentDate = () => new DateTime(2009, 1, 1, 10, 59, 59);
-            Assert.IsTrue(cache.GetItem("Item1", out value));
-            Assert.AreEqual(value, "someValue");
-            Assert.AreEqual(0, cache.CacheItemInvalidations);
+            Assert.True(cache.GetItem("Item1", out value));
+            Assert.Equal(value, "someValue");
+            Assert.Equal(0, cache.CacheItemInvalidations);
 
             // make sure the item does not get evicted at 11:00 because we have touched it a second ago
             cache.GetCurrentDate = () => new DateTime(2009, 1, 1, 11, 00, 00);
-            Assert.IsTrue(cache.GetItem("Item1", out value));
-            Assert.AreEqual(value, "someValue");
-            Assert.AreEqual(0, cache.CacheItemInvalidations);
+            Assert.True(cache.GetItem("Item1", out value));
+            Assert.Equal(value, "someValue");
+            Assert.Equal(0, cache.CacheItemInvalidations);
 
-            Assert.IsNotNull(cache.LruChainHead);
+            Assert.NotNull(cache.LruChainHead);
 
             // make sure the item does not get evicted at 12:00 because we have touched it an hour ago
             cache.GetCurrentDate = () => new DateTime(2009, 1, 1, 12, 00, 00);
-            Assert.IsFalse(cache.GetItem("Item1", out value));
-            Assert.AreEqual(1, cache.CacheItemInvalidations);
+            Assert.False(cache.GetItem("Item1", out value));
+            Assert.Equal(1, cache.CacheItemInvalidations);
 
-            Assert.IsNull(cache.LruChainHead);
-            Assert.IsNull(cache.LruChainTail);
+            Assert.Null(cache.LruChainHead);
+            Assert.Null(cache.LruChainTail);
         }
 
-        [Test]
+        [Fact]
         public void InMemoryCacheCacheExpirationTest3()
         {
             InMemoryCache cache = new InMemoryCache();
@@ -147,67 +146,67 @@ namespace EFCachingProvider.Tests
             cache.GetCurrentDate = () => new DateTime(2009, 1, 1, 10, 2, 0);
 
             // no invalidation happens until we try to get an item
-            Assert.AreEqual("Item4|Item3|Item2|Item1", GetItemKeysInLruOrder(cache));
+            Assert.Equal("Item4|Item3|Item2|Item1", GetItemKeysInLruOrder(cache));
 
-            Assert.IsFalse(cache.GetItem("Item1", out value));
-            Assert.AreEqual("Item4", GetItemKeysInLruOrder(cache));
+            Assert.False(cache.GetItem("Item1", out value));
+            Assert.Equal("Item4", GetItemKeysInLruOrder(cache));
 
         }
 
-        [Test]
+        [Fact]
         public void LruTest1()
         {
             InMemoryCache imc = new InMemoryCache();
-            Assert.AreEqual(Int32.MaxValue, imc.MaxItems);
+            Assert.Equal(Int32.MaxValue, imc.MaxItems);
 
             imc.PutItem("A", "A1", new string[0], TimeSpan.Zero, DateTime.MaxValue);
             imc.PutItem("B", "B1", new string[0], TimeSpan.Zero, DateTime.MaxValue);
             imc.PutItem("C", "C1", new string[0], TimeSpan.Zero, DateTime.MaxValue);
             imc.PutItem("D", "D1", new string[0], TimeSpan.Zero, DateTime.MaxValue);
 
-            Assert.AreEqual("D|C|B|A", GetItemKeysInLruOrder(imc));
+            Assert.Equal("D|C|B|A", GetItemKeysInLruOrder(imc));
 
             object value;
             imc.GetItem("D", out value);
-            Assert.AreEqual("D|C|B|A", GetItemKeysInLruOrder(imc));
+            Assert.Equal("D|C|B|A", GetItemKeysInLruOrder(imc));
             imc.GetItem("C", out value);
-            Assert.AreEqual("C|D|B|A", GetItemKeysInLruOrder(imc));
+            Assert.Equal("C|D|B|A", GetItemKeysInLruOrder(imc));
             imc.GetItem("D", out value);
-            Assert.AreEqual("D|C|B|A", GetItemKeysInLruOrder(imc));
+            Assert.Equal("D|C|B|A", GetItemKeysInLruOrder(imc));
             imc.GetItem("A", out value);
-            Assert.AreEqual("A|D|C|B", GetItemKeysInLruOrder(imc));
+            Assert.Equal("A|D|C|B", GetItemKeysInLruOrder(imc));
             imc.GetItem("B", out value);
-            Assert.AreEqual("B|A|D|C", GetItemKeysInLruOrder(imc));
+            Assert.Equal("B|A|D|C", GetItemKeysInLruOrder(imc));
             imc.InvalidateItem("B");
-            Assert.AreEqual("A|D|C", GetItemKeysInLruOrder(imc));
+            Assert.Equal("A|D|C", GetItemKeysInLruOrder(imc));
             imc.InvalidateItem("C");
-            Assert.AreEqual("A|D", GetItemKeysInLruOrder(imc));
+            Assert.Equal("A|D", GetItemKeysInLruOrder(imc));
             imc.InvalidateItem("A");
-            Assert.AreEqual("D", GetItemKeysInLruOrder(imc));
+            Assert.Equal("D", GetItemKeysInLruOrder(imc));
             imc.InvalidateItem("D");
-            Assert.AreEqual("", GetItemKeysInLruOrder(imc));
+            Assert.Equal("", GetItemKeysInLruOrder(imc));
         }
 
-        [Test]
+        [Fact]
         public void LruWithLimitTest2()
         {
             InMemoryCache imc = new InMemoryCache(3);
 
-            Assert.AreEqual(3, imc.MaxItems);
+            Assert.Equal(3, imc.MaxItems);
 
             imc.PutItem("A", "A1", new string[0], TimeSpan.Zero, DateTime.MaxValue);
             imc.PutItem("B", "B1", new string[0], TimeSpan.Zero, DateTime.MaxValue);
             imc.PutItem("C", "C1", new string[0], TimeSpan.Zero, DateTime.MaxValue);
             imc.PutItem("D", "D1", new string[0], TimeSpan.Zero, DateTime.MaxValue);
 
-            Assert.AreEqual("D|C|B", GetItemKeysInLruOrder(imc));
+            Assert.Equal("D|C|B", GetItemKeysInLruOrder(imc));
             imc.PutItem("E", "E1", new string[0], TimeSpan.Zero, DateTime.MaxValue);
-            Assert.AreEqual("E|D|C", GetItemKeysInLruOrder(imc));
+            Assert.Equal("E|D|C", GetItemKeysInLruOrder(imc));
             imc.PutItem("F", "F1", new string[0], TimeSpan.Zero, DateTime.MaxValue);
-            Assert.AreEqual("F|E|D", GetItemKeysInLruOrder(imc));
+            Assert.Equal("F|E|D", GetItemKeysInLruOrder(imc));
         }
 
-        [Test]
+        [Fact]
         public void DependenciesTest()
         {
             InMemoryCache imc = new InMemoryCache();
@@ -217,13 +216,13 @@ namespace EFCachingProvider.Tests
             imc.PutItem("C", "C1", new string[] { "set2", "set3" }, TimeSpan.Zero, DateTime.MaxValue);
             imc.PutItem("D", "D1", new string[] { "set3", "set1" }, TimeSpan.Zero, DateTime.MaxValue);
 
-            Assert.AreEqual("D|C|B|A", GetItemKeysInLruOrder(imc));
+            Assert.Equal("D|C|B|A", GetItemKeysInLruOrder(imc));
 
             imc.InvalidateSets(new string[] { "set2" });
-            Assert.AreEqual("D|A", GetItemKeysInLruOrder(imc));
+            Assert.Equal("D|A", GetItemKeysInLruOrder(imc));
         }
 
-        [Test]
+        [Fact]
         public void DependenciesTest2()
         {
             InMemoryCache imc = new InMemoryCache();
@@ -233,13 +232,13 @@ namespace EFCachingProvider.Tests
             imc.PutItem("C", "C1", new string[] { "set2", "set3" }, TimeSpan.Zero, DateTime.MaxValue);
             imc.PutItem("D", "D1", new string[] { "set3", "set1" }, TimeSpan.Zero, DateTime.MaxValue);
 
-            Assert.AreEqual("D|C|B|A", GetItemKeysInLruOrder(imc));
+            Assert.Equal("D|C|B|A", GetItemKeysInLruOrder(imc));
 
             imc.InvalidateSets(new string[] { "set1" });
-            Assert.AreEqual("C", GetItemKeysInLruOrder(imc));
+            Assert.Equal("C", GetItemKeysInLruOrder(imc));
         }
 
-        [Test]
+        [Fact]
         public void DependenciesTest3()
         {
             InMemoryCache imc = new InMemoryCache();
@@ -249,13 +248,13 @@ namespace EFCachingProvider.Tests
             imc.PutItem("C", "C1", new string[] { "set2", "set3" }, TimeSpan.Zero, DateTime.MaxValue);
             imc.PutItem("D", "D1", new string[] { "set3", "set1" }, TimeSpan.Zero, DateTime.MaxValue);
 
-            Assert.AreEqual("D|C|B|A", GetItemKeysInLruOrder(imc));
+            Assert.Equal("D|C|B|A", GetItemKeysInLruOrder(imc));
 
             imc.InvalidateSets(new string[] { "set77" });
-            Assert.AreEqual("D|C|B|A", GetItemKeysInLruOrder(imc));
+            Assert.Equal("D|C|B|A", GetItemKeysInLruOrder(imc));
         }
 
-        [Test]
+        [Fact]
         public void DependenciesTest4()
         {
             InMemoryCache imc = new InMemoryCache();
@@ -265,16 +264,16 @@ namespace EFCachingProvider.Tests
             imc.PutItem("C", "C1", new string[] { "set2", "set3" }, TimeSpan.Zero, DateTime.MaxValue);
             imc.PutItem("D", "D1", new string[] { "set3", "set1" }, TimeSpan.Zero, DateTime.MaxValue);
 
-            Assert.AreEqual("D|C|B|A", GetItemKeysInLruOrder(imc));
+            Assert.Equal("D|C|B|A", GetItemKeysInLruOrder(imc));
             imc.PutItem("B", "C1", new string[] { "set3" }, TimeSpan.Zero, DateTime.MaxValue);
-            Assert.AreEqual("B|D|C|A", GetItemKeysInLruOrder(imc));
+            Assert.Equal("B|D|C|A", GetItemKeysInLruOrder(imc));
 
             imc.InvalidateSets(new string[] { "set3" });
-            Assert.AreEqual("A", GetItemKeysInLruOrder(imc));
+            Assert.Equal("A", GetItemKeysInLruOrder(imc));
 
         }
 
-        [Test]
+        [Fact]
         public void MicroStressTest()
         {
             // run a bunch of concurrent reads and writes, make sure we get no exceptions
@@ -337,7 +336,7 @@ namespace EFCachingProvider.Tests
 
                     if (imc.GetItem(randomKey, out value))
                     {
-                        Assert.AreEqual(randomKey + "_V", value);
+                        Assert.Equal(randomKey + "_V", value);
                     }
                 }
             };
@@ -398,7 +397,7 @@ namespace EFCachingProvider.Tests
                 lastEntry = ce;
             }
 
-            Assert.AreSame(lastEntry, imc.LruChainTail);
+            Assert.Same(lastEntry, imc.LruChainTail);
 
             return sb.ToString();
         }
